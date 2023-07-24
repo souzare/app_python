3a9b7b82e2a0a737254163cbdf28aef912059fb4
Doctor <renansouza.ds@gmail.com>
2023-07-24 15:49:20 -0300

    Criando uma métrica customizada

--- hello.py
+++ hello.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template
 from prometheus_client import Counter #Prometheus lib
 from prometheus_flask_exporter import PrometheusMetrics
 
+REQUESTS = Counter("requests_total", "Total number of requests made")
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 @app.route('/')
 def index():
+    REQUESTS.inc()
     return render_template('index.html')
