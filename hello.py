c3463b5c2a46acf150c14c32b850937e4dba884e
Doctor <renansouza.ds@gmail.com>
2023-07-24 15:47:42 -0300

    Mudando o monitoramento

--- hello.py
+++ hello.py
@@ -1,24 +1,9 @@
 from flask import Flask, render_template
 from prometheus_client import Counter #Prometheus lib
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
-
+metrics = PrometheusMetrics(app)
 @app.route('/')
 def index():
     return render_template('index.html')
-
-#Prometheus monitoring
-c = Counter('my_failures', 'Description of counter')
-c.inc()     # Increment by 1
-c.inc(1.6)  # Increment by given value
-
-@c.count_exceptions()
-def f():
-  pass
-
-with c.count_exceptions():
-  pass
-
-# Count only one type of exception
-with c.count_exceptions(ValueError):
-  pass
