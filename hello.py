aa991c4cd411576e11f9e5e044e52b276dac3815
Doctor <renansouza.ds@gmail.com>
2023-07-24 15:43:07 -0300

    Adicionando o Monitoramento

--- hello.py
+++ hello.py
@@ -11,3 +11,14 @@ def index():
 c = Counter('my_failures', 'Description of counter')
 c.inc()     # Increment by 1
 c.inc(1.6)  # Increment by given value
+
+@c.count_exceptions()
+def f():
+  pass
+
+with c.count_exceptions():
+  pass
+
+# Count only one type of exception
+with c.count_exceptions(ValueError):
+  pass
