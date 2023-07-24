b0bd23757e1de8912656bfa1642999fbf679025d
Doctor <renansouza.ds@gmail.com>
2023-07-24 15:40:52 -0300

    Adicionando o Prometheus

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+from prometheus_client import Counter #Prometheus lib
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+#Prometheus monitoring
+c = Counter('my_failures', 'Description of counter')
+c.inc()     # Increment by 1
+c.inc(1.6)  # Increment by given value
