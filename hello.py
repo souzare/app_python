fba4417bbdcbf322e950ae54aff76f3e2d89c130
Doctor <renansouza.ds@gmail.com>
2023-07-25 10:31:49 -0300

    Mudando o site

--- hello.py
+++ hello.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
+import sqlite3
 from prometheus_client import Counter #Prometheus lib
 from prometheus_flask_exporter import PrometheusMetrics
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 REQUESTS = Counter("requests_total", "Total number of requests made")
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 @app.route('/')
 def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
     REQUESTS.inc()
     return render_template('index.html')
