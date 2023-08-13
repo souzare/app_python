8d2eeece9f551573d54958e476bb4b4ec0c4399e
Doctor <renansouza.ds@gmail.com>
2023-08-12 21:03:30 -0300

    instrumnetando a aplicação

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import sqlite3
 import os
-
+from prometheus_client import Counter, generate_latest
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -11,12 +11,23 @@ def get_db_connection():
 
 app = Flask(__name__)
 
+# Definindo a métrica Counter para contagem de solicitações HTTP
+http_requests_total = Counter(
+    'http_requests_total',
+    'Total number of HTTP requests',
+    ['endpoint', 'http_status']
+)
+
 @app.route('/')
 def index():
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
     return render_template('index.html', posts=posts)
+
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype='text/plain')
 
 app.run(host='0.0.0.0', port=5000)
 
