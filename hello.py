2bbd8b00ab2ac777e78bb5172a748b986ac15c1c
Doctor <renansouza.ds@gmail.com>
2023-08-12 21:17:13 -0300

    removendo a instrumentação

--- hello.py
+++ hello.py
@@ -1,23 +1,19 @@
 from flask import Flask, render_template
 import sqlite3
 import os
-from prometheus_client import Counter #Prometheus lib
-from prometheus_flask_exporter import PrometheusMetrics
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
-REQUESTS = Counter("http_requests_total", "Total number of requests made") #metricas do prmetheus
+
 app = Flask(__name__)
-metrics = PrometheusMetrics(app)
 @app.route('/')
 def index():
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
-    REQUESTS.inc() #metricas do prmetheus
     return render_template('index.html', posts=posts)
 
 app.run(host='0.0.0.0', port=5000)
