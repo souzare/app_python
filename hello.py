385d38dac4be83b841180119456c2d7a9409a9f2
Doctor <renansouza.ds@gmail.com>
2023-08-12 21:28:09 -0300

    adicionando a instrumentacao

--- hello.py
+++ hello.py
@@ -1,19 +1,23 @@
 from flask import Flask, render_template
 import sqlite3
 import os
+from prometheus_client import Counter #Prometheus lib
+from prometheus_flask_exporter import PrometheusMetrics
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
-
+REQUESTS = Counter("http_request_total", "Total number of http requests")#Metrica Counter do Prometheus
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 @app.route('/')
 def index():
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
+    REQUESTS.inc() #retorno da metrica
     return render_template('index.html', posts=posts)
 
 app.run(host='0.0.0.0', port=5000)
