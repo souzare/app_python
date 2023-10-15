9b6098b65b340877ba9e064c2933438889e722c2
Doctor <renansouza.ds@gmail.com>
2023-10-15 14:25:46 -0300

    alterando o elastic

--- app.py
+++ app.py
@@ -2,14 +2,12 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 import sqlite3
 import os
-from elasticapm.contrib.flask import ElasticAPM
 app = Flask(__name__)
-apm = ElasticAPM(app)
 
 #monitoramento Elastic
 from elasticapm.contrib.flask import ElasticAPM
 app.config['ELASTIC_APM'] = {
-  'SERVICE_NAME': 'my-service-name',
+  'SERVICE_NAME': 'Python',
 
   'SECRET_TOKEN': 'ePqEgkp1QESVHzKNwK',
 
