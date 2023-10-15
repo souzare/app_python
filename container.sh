adda38d900e342098dd06edc839d539dc45d1c92
Doctor <renansouza.ds@gmail.com>
2023-10-15 14:15:52 -0300

    alterando o ip

--- container.sh
+++ container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker rm -f python
 docker image build -t flask_docker .
-docker run --name "python" -p 5000:5000 --network=app_node_default -d flask_docker
+docker run --name "python" -p 5000:5000 -d flask_docker
