139bdedafa74c7ca1f30cd3db72dd10031b52478
Doctor <renansouza.ds@gmail.com>
2023-10-15 14:59:56 -0300

    alterando o nome

--- app2.py
+++ app2.py
@@ -5,13 +5,11 @@ app = Flask(__name__)
 
 #monitoramento Elastic
 app.config['ELASTIC_APM'] = {
-  'SERVICE_NAME': 'Python',
+  'SERVICE_NAME': 'python',
 
   'SECRET_TOKEN': 'ePqEgkp1QESVHzKNwK',
 
-  'SERVER_URL': 'https://83bdca7098464ab69ad360be80950c3d.apm.us-east-2.aws.elastic-cloud.com:443',
-
-  'ENVIRONMENT': 'Dev',
+  'SERVER_URL': 'https://83bdca7098464ab69ad360be80950c3d.apm.us-east-2.aws.elastic-cloud.com:443'
 }
 apm = ElasticAPM(app)
 
