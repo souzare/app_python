f18ec0dc2482b5282c6af6127f74c19a431699fa
Renan <renan.de.souza@accenture.com>
2025-01-31 10:35:05 -0300

    Alterando a latencia

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def index():
     REQUESTS_COUNT.add(1)
     POSTS_COUNT.set(len(posts))
 
-    with LATENCY.record(amount=1):
+    with LATENCY.time():
         return render_template('index.html', posts=posts)
 
 @app.route('/<int:post_id>')
